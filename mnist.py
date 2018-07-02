39db9b692ec15b229aaeff87b3f308f7a04bbc05
Jean-Yves Pasquier <jean-yves.pasquier@recast.ai>
2018-07-02 19:40:01 +0200

    add training summary

--- mnist.py
+++ mnist.py
@@ -14,11 +14,6 @@ x_train = feature_train.reshape((60000, 28 * 28)).astype(np.float32)
 x_test = feature_test.reshape((-1, 28 * 28)).astype(np.float32)
 y_test = label_test.astype(np.float32)
 
-print(feature_train.shape)
-print(label_train.shape)
-print(x_train.shape)
-print(y_train.shape)
-
 x = tf.placeholder(tf.float32, [None, 784], name="x")
 y_true = tf.placeholder(tf.float32, [None, 10], "y_true")
 y_true_test = tf.placeholder(tf.float32, [None], "y_true_test")
@@ -57,27 +52,39 @@ cost = tf.nn.softmax_cross_entropy_with_logits_v2(labels=y_true, logits=y_pred)
 consolidated_cost = tf.reduce_mean(cost)
 train_step = tf.train.GradientDescentOptimizer(0.001).minimize(cost)
 
+# test
 correct_prediction = tf.equal(tf.cast(tf.argmax(y_pred, axis=1), tf.float32), y_true_test)
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 tf.summary.scalar("testing acuracy", accuracy)
 merged = tf.summary.merge_all()
 
+# training summary
 cost_sum = tf.summary.scalar("cost", consolidated_cost)
+truth_training = tf.cast(tf.argmax(y_true, axis=1), tf.float32)
+acc_training = tf.reduce_mean(tf.cast(tf.equal(tf.cast(tf.argmax(y_pred, axis=1),
+                                                       tf.float32),
+                                               truth_training),
+                                      tf.float32))
+acc_training_sum = tf.summary.scalar("training accuracy", acc_training)
 
-nb_epoch = 1000
-batch_size = 512
-n_batch = int(floor(x_train.shape[0] / batch_size))
-with tf.Session() as sess:
-  sess.run(tf.global_variables_initializer())
-  writer = tf.summary.FileWriter("./.logs", graph=sess.graph)
-  for i in range(nb_epoch):
-    costs = []
-    for b in range(n_batch):
-      _, cost_sum_eval = sess.run([train_step, cost_sum], feed_dict={x: x_train[b * batch_size: (b + 1) * batch_size],
-                                                  y_true: y_train[b * batch_size: (b + 1) * batch_size]})
 
-    acc, merged_summary = sess.run([accuracy, merged], feed_dict={x: x_test,
-                                                                  y_true_test: y_test})
-    writer.add_summary(merged_summary, global_step=i)
-    writer.add_summary(cost_sum_eval, global_step=i)
-    print(i, acc)
+def training(dir="./.logs"):
+  nb_epoch = 1000
+  batch_size = 512
+  n_batch = int(floor(x_train.shape[0] / batch_size))
+  with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    writer = tf.summary.FileWriter(dir, graph=sess.graph)
+    for i in range(nb_epoch):
+      for b in range(n_batch):
+        _, cost_sum_eval = sess.run([train_step, cost_sum],
+                                    feed_dict={x: x_train[b * batch_size: (b + 1) * batch_size],
+                                               y_true: y_train[b * batch_size: (b + 1) * batch_size]})
+
+      acc_training_sumed = sess.run(acc_training_sum, feed_dict={x: x_train, y_true: y_train})
+
+      acc, merged_summary = sess.run([accuracy, merged], feed_dict={x: x_test,
+                                                                    y_true_test: y_test})
+      writer.add_summary(merged_summary, global_step=i)
+      writer.add_summary(cost_sum_eval, global_step=i)
+      writer.add_summary(acc_training_sumed, global_step=i)
